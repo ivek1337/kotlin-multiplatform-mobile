e0d919cc9b09bc50fe4cc120b8daf588e9ea34cf
Ivan Kozić <56771159+ivek1337@users.noreply.github.com>
2024-11-25 15:10:35 +0100

    Update build.gradle.kts

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -16,7 +16,7 @@ kotlin {
         val androidMain by getting
         //val androidTest by getting
         val iosX64Main by getting
-        val iosArm64Main by getting
+        //val iosArm64Main by getting
         val iosSimulatorArm64Main by getting
         val iosMain by creating {
             dependsOn(commonMain)
@@ -25,12 +25,12 @@ kotlin {
             iosSimulatorArm64Main.dependsOn(this)
         }
         val iosX64Test by getting
-        val iosArm64Test by getting
+        //val iosArm64Test by getting
         val iosSimulatorArm64Test by getting
         val iosTest by creating {
             dependsOn(commonTest)
             iosX64Test.dependsOn(this)
-            iosArm64Test.dependsOn(this)
+            //iosArm64Test.dependsOn(this)
             iosSimulatorArm64Test.dependsOn(this)
         }
     }
